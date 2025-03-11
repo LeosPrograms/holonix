33e9266ed4d6f0a2fe685199a81b0e0861521388
Leo Bensman <leobensman@gmail.com>
2025-03-11 22:48:43 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:maackle/holochain/raft-0.4";
+      url = "github:LeosPrograms/holochain/raft-0.4";
       flake = false;
     };
 
